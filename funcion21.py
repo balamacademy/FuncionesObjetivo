c8168abd65645202db87743823112aad0f651199
IsabellaJB <isabellajib5@gmail.com>
2024-05-16 11:14:02 -0600

    Update funcion21.py

--- funcion21.py
+++ funcion21.py
@@ -11,24 +11,42 @@ def shekel_function(x, y, C, beta):
 
 
 def plot_shekel():
-    C = [np.array([1, 2]), np.array([-1, -1]), np.array([2, -2])]
-    beta = [0.1, 0.2, 0.2]
+    # C = [np.array([1, 2]), np.array([-1, -1]), np.array([2, -2])]
+    # beta = [0.1, 0.2, 0.2]
 
-    x = np.linspace(-3, 3, 100)
-    y = np.linspace(-3, 3, 100)
-    x, y = np.meshgrid(x, y)
-    z = np.array([shekel_function(x_, y_, C, beta) for x_, y_ in zip(np.ravel(x), np.ravel(y))])
-    z = z.reshape(x.shape)
+    # x = np.linspace(-3, 3, 100)
+    # y = np.linspace(-3, 3, 100)
+    # x, y = np.meshgrid(x, y)
+    # z = np.array([shekel_function(x_, y_, C, beta) for x_, y_ in zip(np.ravel(x), np.ravel(y))])
+    # z = z.reshape(x.shape)
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # surf = ax.plot_surface(x, y, z, cmap='viridis', edgecolor='none')
+    # ax.set_title('Shekel Function (Simplified)')
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('f(x, y)')
+    # fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    # plt.show()
+    
+    
+    C = np.array([[4, 4], [1, 1], [8, 8], [6, 6], [3, 7]])
+    beta = np.array([0.1, 0.2, 0.2, 0.4, 0.4])
+    x = np.linspace(-10, 10, 100)
+    y = np.linspace(-10, 10, 100)
+    X, Y = np.meshgrid(x, y)
+
+    Z = np.array([[shekel_function(x, y, C, beta) for x, y in zip(X_row, Y_row)] for X_row, Y_row in zip(X, Y)])
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    surf = ax.plot_surface(x, y, z, cmap='viridis', edgecolor='none')
-    ax.set_title('Shekel Function (Simplified)')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('f(x, y)')
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-
+    ax.plot_surface(X, Y, Z, cmap='viridis')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Valor de la función de Shekel')
+    ax.set_title('Función de Shekel modificada')
     plt.show()
 
 
